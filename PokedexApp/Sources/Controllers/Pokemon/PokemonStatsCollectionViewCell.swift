82ac117f8be094bdc94f8a70d19fe5cdfa2bf6f9
Vitaliy Yefimchuk <fry2000.3000@gmail.com>
2021-06-21 22:13:49 +0300

    Set card animation and collectionView with pokemon stats

---
+++ PokedexApp/Sources/Controllers/Pokemon/PokemonStatsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonStatsCollectionViewCell.swift
+//  PokedexApp
+//
+//  Created by Vitaliy Yefimchuk on 21.06.2021.
+//
+
+import UIKit
+
+class PokemonStatsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var statsLabel: UILabel!
+    
+}
